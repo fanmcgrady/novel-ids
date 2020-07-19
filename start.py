489eef3602835d53c187e7bf41620d7f52a0fecf
JohnsonGunn <891833723@qq.com>
2020-07-19 09:49:14 +0800

    降低Q值

--- start.py
+++ start.py
@@ -25,7 +25,7 @@ args = parser.parse_args()
 
 # 可变参数
 feature_number = 41  # 特征总数量
-feature_max_count = args.max_feature  # 选取的特征数目大于该值时，reward为0，用于当特征数目在该范围内时，成功率最多可以到达多少
+feature_max_count = args.maxf  # 选取的特征数目大于该值时，reward为0，用于当特征数目在该范围内时，成功率最多可以到达多少
 MAX_EPISODE = 1000
 net_layers = [64,32]
 
